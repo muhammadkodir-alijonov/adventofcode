9de9d55d4964b99acab19c49eb9c50b84ff4e4f1
muxammadqodir.alijonov <muxammadqodir.alijonov@fido.uz>
2024-12-02 14:31:03 +0500

    2.2

--- 2024/day2/2.1.py
+++ 2024/day2/2.1.py
@@ -1,6 +1,5 @@
 def reports(file_name):
     count = 0
-    i = 0
     try:
         with open(file_name, "r") as file:
             for line in file:
@@ -8,17 +7,31 @@ def reports(file_name):
                 n = len(numbers)
                 if n < 2:
                     continue
-                is_incresing = all(1<= numbers[i] - numbers[i + 1]<=3 for i in range(n - 1))
-                is_decresing = all(1<= numbers[i+1] - numbers[i]<=3 for i in range(n - 1))
-                if is_incresing or is_decresing:
-                    count += 1
+                count += is_safe(numbers)
     except FileNotFoundError:
         print("Fayl topilmadi.")
         return None
     return count
+
+def is_safe(nums):
+    inc = nums[1] > nums[0]
+    if inc:
+        for i in range(1, len(nums)):
+            dif = nums[i] - nums[i - 1]
+            if not (1 <= dif <= 3):
+                return False
+        return True
+    else:
+        for i in range(1, len(nums)):
+            dif = nums[i - 1] - nums[i]
+            if not (1 <= dif <= 3):
+                return False
+        return True
 
 file_name = "input.txt"
 
 result = reports(file_name)
 if result is not None:
     print(f"Shartga mos keluvchi qatorlar soni: {result}")
+
+
---
+++ 2024/day2/2.2.py
@@ -0,0 +1,45 @@
+from bisect import insort_right
+
+
+def reports(file_name):
+    count = 0
+    try:
+        with open(file_name, "r") as file:
+            for line in file:
+                numbers = list(map(int, line.split()))
+                n = len(numbers)
+                if n < 2:
+                    continue
+                count += is_realy_safe(numbers)
+    except FileNotFoundError:
+        print("Fayl topilmadi.")
+        return None
+    return count
+
+def is_safe(nums):
+    inc = nums[1] > nums[0]
+    if inc:
+        for i in range(1, len(nums)):
+            dif = nums[i] - nums[i - 1]
+            if not (1 <= dif <= 3):
+                return False
+        return True
+    else:
+        for i in range(1, len(nums)):
+            dif = nums[i - 1] - nums[i]
+            if not (1 <= dif <= 3):
+                return False
+        return True
+def is_realy_safe(nums):
+    for i in range(len(nums)):
+        if is_safe(nums[:i] + nums[i + 1:]):
+            return True
+    return False
+
+file_name = "input.txt"
+
+result = reports(file_name)
+if result is not None:
+    print(f"Shartga mos keluvchi qatorlar soni: {result}")
+
+
