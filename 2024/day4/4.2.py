436217069ac54282c43e652d6728b904594dfa1e
muxammadqodir.alijonov <muxammadqodir.alijonov@fido.uz>
2024-12-04 14:50:40 +0500

    4.2

--- 2024/day4/4.2.py
+++ 2024/day4/4.2.py
@@ -1,34 +1,27 @@
-import re
+with open("input.txt") as fin:
+    lines = fin.readlines()
+
+n = len(lines)
+m = len(lines[0])
 
 
-def process_memory(memory):
-    mul_enabled = True
-    total_sum = 0
+def has_xmas(i, j):
+    if not (1 <= i < n - 1 and 1 <= j < m - 1):
+        return False
+    if lines[i][j] != "A":
+        return False
 
-    mul_pattern = r"mul\((\d+),(\d+)\)"
-    do_pattern = r"do\(\)"
-    dont_pattern = r"don't\(\)"
+    diag_1 = [lines[i - 1][j - 1], lines[i + 1][j + 1]]
+    diag_2 = [lines[i - 1][j + 1], lines[i + 1][j - 1]]
 
-    tokens = re.finditer(r"do\(\)|don't\(\)|mul\(\d+,\d+\)", memory)
-
-    for token in tokens:
-        match = token.group(0)
-
-        if match == "do()":
-            mul_enabled = True
-        elif match == "don't()":
-            mul_enabled = False
-        elif match.startswith("mul("):
-            nums = re.match(mul_pattern, match)
-            if nums and mul_enabled:
-                x, y = map(int, nums.groups())
-                total_sum += x * y
-
-    return total_sum
+    # Diagonallarni tekshirish
+    return "".join(diag_1) in ["MS", "SM"] and "".join(diag_2) in ["MS", "SM"]
 
 
-with open('input.txt', 'r') as file:
-    memory = file.read()
 
-result = process_memory(memory)
-print(result)
+ans = 0
+for i in range(n):
+    for j in range(m):
+        ans += has_xmas(i, j)
+
+print(ans)
