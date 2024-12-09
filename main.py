4d1146972056dc3bdb358a543a7a0723501f215d
Muhammadqodir Alijonov <alijonov0608@gmail.com>
2024-12-09 16:21:33 +0500

    day 9 done

--- main.py
+++ main.py
@@ -1,31 +1,9 @@
-import bisect
+from tqdm import tqdm
+import time
 
-def maxValue(events):
-    events.sort(key=lambda x: x[1])
-
-    n = len(events)
-
-    dp = [0] * n
-    dp[0] = events[0][2]
-
-    for i in range(1, n):
-        dp[i] = max(dp[i-1], events[i][2])  
-
-    def find_last_non_overlapping(i):
-        low, high = 0, i-1
-        while low <= high:
-            mid = (low + high) // 2
-            if events[mid][1] < events[i][0]:
-                low = mid + 1
-            else:
-                high = mid - 1
-        return high
-
-    max_sum = dp[-1] 
-
-    for i in range(1, n):
-        prev_index = find_last_non_overlapping(i)
-        if prev_index >= 0:
-            max_sum = max(max_sum, dp[prev_index] + events[i][2])
-
-    return max_sum
+# Sonlar o'sishini kuzatish
+for i in tqdm(range(1, 1001), desc="Sonlar o'sishi", ncols=100, position=0):
+    # Agar son 10000 ga bo'lsa, chiqarish
+    if i % 10000 == 0:
+        print(i)
+    time.sleep(0.01)  # Vaqtni kechiktirish (jarayonni sekinlashtirish uchun)
