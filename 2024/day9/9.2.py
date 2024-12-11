e12941f271a80a6fc49be4a3890f2572f6a35d50
Muhammadqodir Alijonov <alijonov0608@gmail.com>
2024-12-11 21:48:37 +0500

    change 9.2 done

--- 2024/day9/9.2.py
+++ 2024/day9/9.2.py
@@ -1,80 +1,96 @@
 def read_input(path):
     try:
         with open(path) as fin:
-            return fin.read().strip()
+            map_ = fin.read().strip()
+            return map_
     except FileNotFoundError:
         print(f"Error: File '{path}' not found.")
         return ""
 
-def make_filesystem(diskmap):
-    loc = [0] * len(diskmap)
-    size = [0] * len(diskmap)
-    blocks = []
 
-    id = 0
+def make_up_amphipod(map_):
+    amphipod = []
     is_file = True
-    for x in diskmap:
-        x = int(x)
+    file_id = 0
+
+    for length in map_:
+        length = int(length)
         if is_file:
-            loc[id] = len(blocks)
-            size[id] = x
-            blocks += [id] * x
-            id += 1
-            is_file = False
+            amphipod.extend([str(file_id)] * length)
+            file_id += 1
         else:
-            blocks += [None] * x
-            is_file = True
+            amphipod.extend(['.'] * length)
+        is_file = not is_file
 
-    return blocks, loc, size
+    return amphipod
 
-def move_files(filesystem, loc, size):
-    big = 0
-    while size[big] > 0:
-        big += 1
-    big -= 1
 
-    for to_move in range(big, -1, -1):
-        file_size = size[to_move]
-        first_free = 0
-        free_space = 0
+def find_free_spans(amphipod):
+    spans = []
+    start = None
+    for i, block in enumerate(amphipod):
+        if block == '.':
+            if start is None:
+                start = i
+        else:
+            if start is not None:
+                spans.append((start, i - 1))
+                start = None
+    if start is not None:
+        spans.append((start, len(amphipod) - 1))
+    return spans
 
-        while first_free < loc[to_move] and free_space < file_size:
-            first_free += free_space
-            free_space = 0
-            while filesystem[first_free] is not None:
-                first_free += 1
-            while first_free + free_space < len(filesystem) and filesystem[first_free + free_space] is None:
-                free_space += 1
 
-        if first_free >= loc[to_move]:
-            continue
+def move_files_whole(amphipod):
+    file_ids = sorted({int(block) for block in amphipod if block.isnumeric()}, reverse=True)
+    for file_id in file_ids:
+        file_indices = [i for i, block in enumerate(amphipod) if block == str(file_id)]
+        file_length = len(file_indices)
 
-        for idx in range(first_free, first_free + file_size):
-            filesystem[idx] = to_move
-        for idx in range(loc[to_move], loc[to_move] + file_size):
-            filesystem[idx] = None
+        # Find all free spans
+        free_spans = find_free_spans(amphipod)
 
-    return filesystem
+        # Look for the leftmost suitable span that can fit the current file
+        best_span = None
+        for start, end in free_spans:
+            if end - start + 1 >= file_length:
+                best_span = (start, start + file_length - 1)
+                break
 
-def checksum(filesystem):
+        if best_span:
+            start, end = best_span
+            # Move the file to the span
+            for i in range(start, end + 1):
+                amphipod[i] = str(file_id)
+            # Clear the original file positions
+            for idx in file_indices:
+                amphipod[idx] = '.'
+    return amphipod
+
+
+def filesystem_checksum(amphipod):
     total_sum = 0
-    for i, x in enumerate(filesystem):
-        if x is not None:
-            total_sum += i * x
+    for i, block in enumerate(amphipod):
+        if block.isnumeric():
+            total_sum += i * int(block)
     return total_sum
 
-def main():
+
+def main_part_two():
     path = "input.txt"
-    diskmap = read_input(path)
-    if not diskmap:
+    map_ = read_input(path)
+    if not map_:
         return
 
-    filesystem, loc, size = make_filesystem(diskmap)
+    amphipod = make_up_amphipod(map_)
+    print("Initial amphipod:", ''.join(amphipod))
 
-    moved_filesystem = move_files(filesystem, loc, size)
+    compacted_amphipod = move_files_whole(amphipod)
+    print("Compacted amphipod after whole-file moves:", ''.join(compacted_amphipod))
 
-    result_checksum = checksum(moved_filesystem)
-    print(f"Filesystem checksum: {result_checksum}")
+    checksum = filesystem_checksum(compacted_amphipod)
+    print("Filesystem checksum (Part 2):", checksum)
+
 
 if __name__ == "__main__":
-    main()
+    main_part_two()
